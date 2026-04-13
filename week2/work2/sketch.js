ec80a2b79742dddd3148d140ff556c5158367c50
oliivviia <oliivviia04@gmail.com>
2026-04-13 16:23:32 +0900

    Add files via upload

---
+++ week2/work2/sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(225);
+  
+  fill(255,0,0);
+  stroke(0,0,0);
+  strokeWeight(5);
+  ellipse(150,150,250,250);
+  
+  fill(0,0,255,128);
+  stroke(0,0,0);
+  strokeWeight(10);
+  rect(150,150,200,200)
+  
+}
